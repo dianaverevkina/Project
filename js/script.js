a95c247f8ab833dc5a9072e59c69fb77ac17d0bb
dianaverevkina <108188896+dianaverevkina@users.noreply.github.com>
2023-01-09 20:03:09 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -5,7 +5,20 @@
 // if(ibg[i].querySelector('img')){
 // ibg[i].style.backgroundImage = 'url('+ibg[i].querySelector('img').getAttribute('src')+')';
 // }
-// }
+// }x
 // }
 
 // ibg();
+
+let menu__icon = document.querySelectorAll('.menu__icon');
+let menu__body = document.querySelector('.menu__body');
+let back = document.querySelector('body');
+
+menu__icon.forEach(function (item) {
+   item.onclick = function () {
+      item.classList.toggle('active');
+      menu__body.classList.toggle('active');
+      back.classList.toggle('lock');
+   }
+});
+
